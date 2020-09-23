a364e211bb80c3ed9cfbd54f835211fa7b8803ca
chadley <chadleyderose1911201@gmail.com>
2020-09-23 14:14:08 +0200

    update new

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ print("---Welcome To Nedbank Loan Console Calculator---")
 
 loan_amount = int(input("Please enter loan amount: "))
 interest_rate = float(input("Please enter your interest rate without a percentage sign: "))
-_number_of_months = 12  # how long it would take you to repay the loan.
+_number_of_months = 24  # how long it would take you to repay the loan.
 
 total_repayment = loan_amount + (loan_amount * interest_rate)
 monthly_installment = total_repayment / _number_of_months
